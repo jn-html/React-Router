55baa28ab8c6d5a3572397f6ba8c92a6bf89a5bc
jn-html <marmainjean@gmail.com>
2019-10-23 11:04:13 +0200

    Finish react router

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter, Switch, NavLink}
+ from 'react-router-dom';
+ import './App.css';
+ import Home from './components/Home';
+ import History from './components/History'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <BrowserRouter>
+      <div>
+              <NavLink className="navLink" exact to="/">Acceuil</NavLink>
+              <NavLink className="navLink" to="/notre-histoire"> Notre histoire</NavLink>
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/notre-histoire" component={History} />
+              </Switch>
+      </div>
+            </BrowserRouter>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/History.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function History () {
+    return (
+        <div className="History">
+            <p>Notre histoire</p>
+        </div>
+    )
+}
+
+
+export default History;
